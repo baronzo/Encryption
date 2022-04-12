2a7000577655b564370423cc055de926cf8e61c3
jrapbit <67047826+jrapbit@users.noreply.github.com>
2022-04-13 04:48:48 +0700

    updated

--- src/components/algorithm/RSA.vue
+++ src/components/algorithm/RSA.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    RSA
+  <div id="rsa">
+    <div id="title">RSA</div>
+    <div id="box">
+      <div class="inside-box">
+        <div class="box-title">Public Key</div>
+        <div class="input-box">
+            <textarea
+                id="input"
+                type="text"
+                maxlength="26"
+                v-model="messageInput"
+            />
+        </div>
+        <div class="box-title">Private Key</div>
+        <div class="input-box">
+            <textarea
+                id="input"
+                type="text"
+                maxlength="26"
+                v-model="messageInput"
+            />
+        </div>
+        <div class="button">Generate Key Pair</div>
+      </div>
+      <div class="inside-box">
+        <div class="box-title">Plain Text</div>
+        <div class="input-box">
+            <textarea
+                id="input"
+                type="text"
+                maxlength="26"
+                v-model="messageInput"
+            />
+        </div>
+        <div class="box-title">Encoded</div>
+        <div class="input-box">
+            <textarea
+                id="input"
+                type="text"
+                maxlength="26"
+                v-model="messageInput"
+            />
+        </div>
+        <div class="button">Encrypt</div>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -11,5 +56,5 @@ export default {
 </script>
 
 <style lang="scss">
-  
+ @import "../style/RSA";
 </style>

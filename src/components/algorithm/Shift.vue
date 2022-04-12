74e7360454dad13dee4e4669b998f2bd8b07c67a
at-praew <67423313+at-praew@users.noreply.github.com>
2022-04-12 22:15:06 +0700

    shift function

--- src/components/algorithm/Shift.vue
+++ src/components/algorithm/Shift.vue
@@ -1,52 +1,144 @@
 <template>
   <div id="Mono">
-    <div id="title">Monoalphabetic Substitution Cipher</div>
-    <div id="changeMode">
-      <div id="mode" v-if="mode" @click="mode = !mode">Encoded</div>
-      <div id="mode" v-if="!mode" @click="mode = !mode">Decoded</div>
-    </div>
+    <div id="title">Shift Cipher</div>
     <div id="monoBox">
-      <div id="keyBox">
-        <div id="keyHeader">Key</div>
-        <div id="keyInputBox">
-          <textarea id="keyInput" type="text" maxlength="26" v-model="keyInput"/>
+      <div id="inOutPutBox">
+        <div id="keyBox">
+          <div id="keyHeader">Key</div>
+          <div id="keyInputBox">
+            <textarea
+              id="keyInput"
+              type="text"
+              v-model="keyInput"
+              @keyup="checkMessage(keyInput)"
+            />
+          </div>
+          <div id="genBox">
+            <div id="genKey" @click="generateKey()">Generate Key</div>
+          </div>
         </div>
-      </div>
-      <div id="messageBox">
-        <div id="pHeader">PText</div>
-        <div id="pInputBox">
-          <textarea id="pInput" type="text" v-model="messageInput" />
+        <div id="messageBox">
+          <div id="pHeader" v-if="mode">Plain Text</div>
+          <div id="pHeader" v-if="!mode">Encoded</div>
+          <div id="pInputBox">
+            <textarea
+              id="pInput"
+              type="text"
+              v-model="messageInput"
+              maxlength="26"
+            />
+          </div>
+          <div id="swapBox">
+            <div id="swap" class="swapIcon" @click="changeMode()">
+              <i id="iSwap" class="fas fa-arrows-alt-v"></i>
+              <i id="iSwap" class="fas fa-arrow-down"></i>
+            </div>
+          </div>
+          <div id="cHeader" v-if="mode">Encoded</div>
+          <div id="cHeader" v-if="!mode">Plain Text</div>
+          <div id="cValueBox">
+            <textarea
+              id="cInput"
+              type="text"
+              v-model="encryptInput"
+              :disabled="true"
+            />
+          </div>
+          <div id="buttonBox">
+            <div id="buttonMono" @click="selectMode()" v-if="mode">Encrypt</div>
+            <div id="buttonMono" @click="selectMode()" v-if="!mode">
+              Decrypt
+            </div>
+          </div>
         </div>
-        <div id="cHeader" v-if="mode">Decoded</div>
-        <div id="cHeader" v-if="!mode">Encoded</div>
-        <div id="cValueBox">
-          <div id="cValue"></div>
-        </div>
-        <div id="buttonBox">Done</div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  alpha: "abcdefghijklmnopqrstuvwxyz",
+import { defineComponent } from "vue";
+export default defineComponent({
   name: "MonoComponent",
   data() {
     return {
-      mode: false,
+      mode: true,
       keyInput: "",
       messageInput: "",
+      encryptInput: "",
+      alpha: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     };
   },
-  method: {
-    encrypt() {
-      
+  methods: {
+    selectMode() {
+      if (this.mode) {
+        this.encrypt();
+      } else {
+        this.decrypt();
+      }
     },
-  }
-};
+    encrypt() {
+      let message = this.messageInput.toUpperCase();
+      let keyNumber = Number(this.keyInput);
+      let answer = "";
+      for (let index = 0; index < message.length; index++) {
+          if (this.messageInput[index] == " ") {
+          answer += " ";
+        } else {
+          answer += this.alpha[(this.alpha.indexOf(message[index]) + keyNumber) % 26];
+        }
+      }
+      this.encryptInput = answer; 
+    },
+    decrypt() {
+      let message = this.messageInput.toUpperCase();
+      let keyNumber = Number(this.keyInput);
+      let answer = "";
+      for (let index = 0; index < message.length; index++) {
+          if (this.messageInput[index] == " ") {
+          answer += " ";
+        } else {
+          if (this.alpha.indexOf(message[index]) - keyNumber < 0) {
+          answer += this.alpha[(this.alpha.indexOf(message[index]) - keyNumber) + 26];  
+          }else {
+            answer += this.alpha[(this.alpha.indexOf(message[index]) - keyNumber)];
+        }
+          }
+      }
+      this.encryptInput = answer; 
+    },
+    changeMode() {
+      this.mode = !this.mode;
+      let first, second;
+      first = this.messageInput;
+      second = this.encryptInput;
+      this.messageInput = second;
+      this.encryptInput = first;
+    },
+    generateKey() {
+      let key = "";
+      let message = this.messageInput.replace(/\s/g, "");
+      while (key.length < message.length) {
+        let rndInt = Math.random() * 26 + 1;
+        rndInt = parseInt(String(rndInt));
+        if (key.includes(this.alpha[rndInt - 1])) {
+          continue;
+        } else {
+          key += this.alpha[rndInt - 1];
+        }
+      }
+      this.keyInput = key;
+    },
+    checkMessage(key:string) {
+      let message = this.messageInput.replace(/\s/g, "");
+      if (key.length > message.length) {
+        this.keyInput = key.substring(0, key.length - 1);
+      }
+    },
+  },
+});
 </script>
 
 <style lang="scss">
-
+@import "../style/Shift.scss";
 </style>
